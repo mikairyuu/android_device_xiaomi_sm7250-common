e06c127adf38c36d5a56c34c1485fc7d29edd38f
xSylla <syllamodder@gmail.com>
2021-06-15 13:02:53 +0200

    Revert "sm8250-common: Add power AIDL DT2W support via sysfs node"
    This reverts commit 9db0864cb761a8b831cead05b4b8958b99a3afa7.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -133,9 +133,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := lito
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
-
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
