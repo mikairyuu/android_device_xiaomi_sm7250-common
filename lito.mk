204a05d90426429d8c8d5be3e4425b465fdd0210
xSylla <syllamodder@gmail.com>
2021-07-01 21:11:44 +0200

    sm7250-common: Migrate to Xiaomi power AIDL HAL
    Signed-off-by: xSylla <syllamodder@gmail.com>

--- lito.mk
+++ lito.mk
@@ -304,8 +304,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service.xiaomi-libperfmgr
 
 # Public libraries
 PRODUCT_COPY_FILES += \
