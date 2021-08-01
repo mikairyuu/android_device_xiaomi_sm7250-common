6c87f5b7002ffa1c8243c653b82b547bfb651676
xSylla <syllamodder@gmail.com>
2021-08-01 09:26:19 +0200

    sm7250-common: Add gauguin to the list of supported devices
    Signed-off-by: xSylla <syllamodder@gmail.com>

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter monet picasso vangogh,$(TARGET_DEVICE)),)
+ifneq ($(filter monet picasso vangogh gauguin,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
