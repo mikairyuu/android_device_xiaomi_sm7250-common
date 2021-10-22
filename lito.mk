d2e7b594d5749da5ad6f69091e5fe058e1b19abe
Roberto Sartori <roberto.sartori.android@gmail.com>
2021-10-22 15:19:51 +0530

    sm7250-common: disable compressed APEX
     Compressed APEX has been introduced in Android 12, and they are enabled
     by default inheriting updatable_apex.mk.
     Unfortunately, when the device is encrypted, they are causing bootloops.
    
     As the only advantage is some space gain on disk, we can
     simply disable them: keep the variable immediatly after including the
     updatable_apex.mk to avoid being overwritten.

--- lito.mk
+++ lito.mk
@@ -18,6 +18,10 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Car
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Disable APEX compression
+# Keep this after including updatable_apex.mk
+PRODUCT_COMPRESSED_APEX := false
+
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
