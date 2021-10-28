32e56cbe9503e30bbbe0098a0699a8e3b97600a9
xSylla <syllamodder@gmail.com>
2021-10-28 14:27:11 +0200

    [DNM]sm7250-common: TMP

--- lito.mk
+++ lito.mk
@@ -255,8 +255,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+#PRODUCT_BOOT_JARS += \
+    #org.ifaa.android.manager
 
 # IPACM
 PRODUCT_PACKAGES += \
@@ -470,8 +470,8 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+    #WfdCommon
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm7250-common/sm7250-common-vendor.mk)
