b48546cb3d89186be222a549efa11622b73b8d72
xSylla <syllamodder@gmail.com>
2021-11-11 10:04:33 +0100

    Partially revert "sm7250-common: Import audio_policy_configuration_odm from V12.5.1.0.RJIMIXM"
    *AOSP load only one audio configuration, so let's import the right conf and delete the second one
    *Config file is already updated with all changes made before
    
    This reverts commit 773dad0de55836146913b86dd606c5f516d2592a.

--- lito.mk
+++ lito.mk
@@ -123,7 +123,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml
 
 PRODUCT_COPY_FILES += \
