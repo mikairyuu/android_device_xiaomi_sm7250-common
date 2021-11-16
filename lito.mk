09a042dd0e7c4cb27973fb23603d51230641269a
mikairyuu <miva200267@gmail.com>
2021-11-16 11:06:30 +1000

    sm7250-common: Build android.hardware.secure_element@1.0

--- lito.mk
+++ lito.mk
@@ -365,7 +365,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.6.vendor \
     android.hardware.radio.config@1.3.vendor \
-    android.hardware.radio.deprecated@1.0.vendor
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.secure_element@1.0.vendor
 
 # Rootdir
 PRODUCT_PACKAGES += \
