1b19711be7c41ba895a997624de84152d246c5bb
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-18 09:43:16 +0200

    sm7250-common: Switch to prebuilt secure_element stack

--- lito.mk
+++ lito.mk
@@ -308,7 +308,7 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc_snxxx@1.2-service \
-    android.hardware.secure_element_snxxx@1.2-service \
+    android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
     libchrome.vendor \
     NfcNci \
