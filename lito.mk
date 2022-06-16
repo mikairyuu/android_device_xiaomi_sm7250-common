a9b018a2665cbbf4ad64cd5696bff03529588b52
mikairyuu <miva200267@gmail.com>
2022-06-16 13:12:57 +1000

    sm7250-common: import configs from snow

--- lito.mk
+++ lito.mk
@@ -408,6 +408,9 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
