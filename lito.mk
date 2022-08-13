24811f7a3c0fe5311e717d85b474f64904cc591b
johnmart19 <johnivan19999@gmail.com>
2022-08-14 01:41:15 +0300

    sm7250-common: Audio: Compile Audio Alsa & Debugging Extensions
    - Reference - Miui 13.0.2.0 Monet EU:
    
    vendor/lib/libalsautils.so
    vendor/lib64/libalsautils.so
    
    system/lib/libtinyalsa.so
    system/lib64/libtinyalsa.so
    
    - Update flags according to following system_media/alsa_utils/Android.bp changes
    "add libaudioroutev2 and libalsautilsv2 linking libtinyalsav2"
    [mm libaudioroute libaudioroutev2 libalsautils libalsautilsv2]
    
    - Also compile out default audio debugging packages
    
    * tinymix | tinyplay | tinycap | tinypcminfo | cplay
    * This packages we need to have on eng and userdebug builds to check and verify soundtrigger configuration
    
    - Test:
    * Check that libs compiled, matching stock pack of audio libs and including latest v2 versions
    * Check that TinyMix is working as requested on ENG builds

--- lito.mk
+++ lito.mk
@@ -111,6 +111,23 @@ PRODUCT_PACKAGES += \
     libvisualizer \
     libvolumelistener
 
+# Audio Alsa Extensions
+PRODUCT_PACKAGES += \
+   libaudioroute \
+   libaudioroutev2 \
+   libalsautils \
+   libalsautilsv2
+
+# Audio Debugging Packages
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+PRODUCT_PACKAGES += \
+    tinyplay \
+    tinycap \
+    tinymix \
+    tinypcminfo \
+    cplay
+endif
+
 # Audio configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
