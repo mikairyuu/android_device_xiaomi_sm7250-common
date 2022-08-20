62689537038b60dcf5cdd775780d4809628a4ec4
mikairyuu <miva200267@gmail.com>
2022-08-20 12:16:48 +0300

    sm7250-common: Build Pixel Thermal HAL

--- lito.mk
+++ lito.mk
@@ -414,8 +414,14 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal HAL
+SOONG_CONFIG_NAMESPACES += thermal_hal_feature
+SOONG_CONFIG_thermal_hal_feature += \
+    pid \
+
+SOONG_CONFIG_thermal_hal_feature_pid ?= apply_1_0
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.mock
+    android.hardware.thermal@2.0-service.pixel \
+    thermal_symlinks
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
