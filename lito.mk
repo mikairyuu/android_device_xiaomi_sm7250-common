01f7e7b227703c5357503e18ba565d043e278c4e
mikairyuu <miva200267@gmail.com>
2022-09-04 18:13:35 +0300

    sm7250-common: Build vendor.qti.hardware.btconfigstore@2.0 interface

--- lito.mk
+++ lito.mk
@@ -127,7 +127,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
