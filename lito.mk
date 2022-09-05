1fe339de6019977d9d9a7eb10f892251d50ee31a
mikairyuu <miva200267@gmail.com>
2022-09-05 13:08:37 +0300

    sm7250-common: Build android.hardware.bluetooth.a2dp

--- lito.mk
+++ lito.mk
@@ -80,7 +80,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl \
-    vendor.qti.hardware.audiohalext@1.0.vendor
+    vendor.qti.hardware.audiohalext@1.0.vendor \
+    android.hardware.bluetooth.a2dp@1.0
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
