0bf8ffd1d667a41d8aa6d802c79d264fb2c220cc
mikairyuu <miva200267@gmail.com>
2022-09-05 14:13:35 +0300

    sm7250-common: Build missing bt libs

--- lito.mk
+++ lito.mk
@@ -128,7 +128,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    libbthost_if \
+    libldacBT_bco
 
 # Camera
 PRODUCT_PACKAGES += \
