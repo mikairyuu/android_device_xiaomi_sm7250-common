00df051a5750b017b2ea2cf15d1cf85ad6995c7c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-13 20:10:47 +0100

    sm7250-common: Remove non-existing packages
    Change-Id: Ia154904be35b6d6b65f5879be244d2168354bd7e

--- lito.mk
+++ lito.mk
@@ -179,7 +179,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     libdisplayconfig.qti \
-    libdisplayconfig.qti.vendor \
     libdisplayconfig.system.qti \
     libqdMetaData \
     libqdMetaData.system \
