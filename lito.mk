6b86951b4277a1d1ed56240170b07ef8216cbd62
mikairyuu <miva200267@gmail.com>
2023-01-10 19:46:57 +1000

    sm7250-common: Go back to Aperture

--- lito.mk
+++ lito.mk
@@ -154,7 +154,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    GCamGOPrebuilt-V3 \
     libcamera2ndk_vendor \
     libgui_vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
