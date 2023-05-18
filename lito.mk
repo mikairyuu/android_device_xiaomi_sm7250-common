fd3cead22b5ce44af11e6afdee9ccc8c50f11196
Michael Bestas <mkbestas@lineageos.org>
2023-05-19 00:20:56 +0300

    sm7250-common: Switch to common QTI vibrator HAL
    Change-Id: I281c6d993901ad86592d43afcac2171840998738

--- lito.mk
+++ lito.mk
@@ -457,11 +457,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi_lito
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 # WiFi
 PRODUCT_PACKAGES += \
